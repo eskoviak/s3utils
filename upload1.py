7e6e497f53e28222bf7cae2800b8b87c23ab123a
Ed Skoviak <eskoviak@gmail.com>
2021-12-17 19:10:13 -0600

    initial commit

---
+++ upload1.py
@@ -0,0 +1,27 @@
+import os
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+datafile = os.path.expanduser('~/iCloud/Downloads/Weight History.csv')
+
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                      aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+if __name__ == '__main__':
+    uploaded = upload_to_aws(datafile, os.environ['WEIGHT_HISTORY_BUCKET'],
+	os.environ['WEIGHT_HISTORY_KEY'])
+
+
+
